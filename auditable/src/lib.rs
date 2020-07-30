17cba1dd6c0148c01840d42ca82727b529b0f676
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-07-30 03:26:32 +0200

    Drop auditable::annotate_this_executable() as it doesn't seem to be needed anymore

--- auditable/src/lib.rs
+++ auditable/src/lib.rs
@@ -1,17 +1,8 @@
 #![feature(test)]
 extern crate test;
 
-#[link_section = ".dep-list"]
+#[link_section = ".rust-audit-dep-list"]
 static AUDITABLE_VERSION_INFO: [u8; include_bytes!(concat!(env!("OUT_DIR"), "/Cargo.lock.annotated")).len()] = *include_bytes!(concat!(env!("OUT_DIR"), "/Cargo.lock.annotated"));
-
-/// Call this from main() or other reachable place in the code to annotate your executable
-/// with information on which library versions were used for building it.
-/// 
-/// Calling this function should not incur any performance penalty at runtime.
-#[inline(always)]
-pub fn annotate_this_executable() {
-    test::black_box(AUDITABLE_VERSION_INFO);
-}
 
 #[inline]
 pub fn version_info() -> &'static [u8] {
--- hello-auditable/src/main.rs
+++ hello-auditable/src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-    auditable::annotate_this_executable();
     println!("Hello, world!");
     println!("My version info is:\n{}", std::str::from_utf8(auditable::version_info()).unwrap());
 }
