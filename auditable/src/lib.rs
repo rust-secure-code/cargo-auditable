ee4875ad05b809d9b2abc6b29fea233869cd4efd
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-07-30 08:01:10 +0200

    Drop nightly feature, should now compile on stable

--- auditable/src/lib.rs
+++ auditable/src/lib.rs
@@ -1,6 +1,3 @@
-#![feature(test)]
-extern crate test;
-
 #[link_section = ".rust-audit-dep-list"]
 static AUDITABLE_VERSION_INFO: [u8; include_bytes!(concat!(env!("OUT_DIR"), "/Cargo.lock.annotated")).len()] = *include_bytes!(concat!(env!("OUT_DIR"), "/Cargo.lock.annotated"));
 
