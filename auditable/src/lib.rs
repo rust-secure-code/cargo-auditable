ae61ed763e706b54d67d59c512802f1297529efa
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-07-31 21:35:38 +0200

    a dash of docs

--- auditable/src/lib.rs
+++ auditable/src/lib.rs
@@ -5,6 +5,9 @@ static AUDITABLE_VERSION_INFO: [u8; include_bytes!(concat!(
 ))
 .len()] = *include_bytes!(concat!(env!("OUT_DIR"), "/Cargo.lock.annotated"));
 
+
+/// Returns the version info embedded into the executable at compile time.
+/// You should call `str::from_utf8()` on it to make it usable.
 #[inline]
 pub fn version_info() -> &'static [u8] {
     &AUDITABLE_VERSION_INFO
