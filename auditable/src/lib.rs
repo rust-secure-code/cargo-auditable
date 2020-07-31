6896332f04692b46bda3bca116ffe79d2fa7e087
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-07-31 23:24:27 +0200

    Add #[used] to the static

--- auditable/src/lib.rs
+++ auditable/src/lib.rs
@@ -1,10 +1,10 @@
+#[used]
 #[link_section = ".rust-audit-dep-list"]
 static AUDITABLE_VERSION_INFO: [u8; include_bytes!(concat!(
     env!("OUT_DIR"),
     "/Cargo.lock.annotated"
 ))
 .len()] = *include_bytes!(concat!(env!("OUT_DIR"), "/Cargo.lock.annotated"));
-
 
 /// Returns the version info embedded into the executable at compile time.
 /// You should call `str::from_utf8()` on it to make it usable.
