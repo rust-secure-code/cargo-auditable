53ee959d0dc0fb093f39cbf17acf63f3cec9f35c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-08-01 22:26:13 +0200

    Add custom data structures for a stable format for serializing/deserializing dependency info

---
+++ auditable-serde/src/lib.rs
@@ -0,0 +1,111 @@
+use cargo_lock;
+use std::{str::FromStr, convert::TryInto, error::Error};
+pub struct RawVersionInfo {
+    packages: Vec<Package>
+}
+pub struct Package {
+    name: String,
+    version: String,
+    checksum: String,
+    dependencies: Vec<Dependency>
+}
+pub struct Dependency {
+    name: String,
+    version: String
+}
+
+impl From<cargo_lock::dependency::Dependency> for Dependency {
+    fn from(source: cargo_lock::dependency::Dependency) -> Self {
+        Self {
+            // TODO: get non-copying conversion from Name to String implemented upstream
+            name: source.name.as_str().to_owned(),
+            version: format!("{}", source.version)
+        }
+    }
+}
+
+impl From<cargo_lock::package::Package> for Package {
+    fn from(source: cargo_lock::package::Package) -> Self {
+        Self {
+            // TODO: get non-copying conversion from Name to String implemented upstream
+            name: source.name.as_str().to_owned(),
+            version: format!("{}", source.version),
+            checksum: match source.checksum {
+                Some(value) => format!("{}", value),
+                None => String::new(), // TODO: consider failing?
+            },
+            dependencies: source.dependencies.into_iter().map(|d| d.into()).collect()
+        }
+    }
+}
+
+impl From<cargo_lock::lockfile::Lockfile> for RawVersionInfo {
+    fn from(source: cargo_lock::lockfile::Lockfile) -> Self {
+        Self {
+            packages: source.packages.into_iter().map(|p| p.into()).collect()
+        }
+    }
+    
+}
+
+impl TryInto<cargo_lock::dependency::Dependency> for Dependency {
+    type Error = cargo_lock::error::Error;
+    fn try_into(self) -> Result<cargo_lock::dependency::Dependency, Self::Error> {
+        Ok(cargo_lock::dependency::Dependency {
+            name: cargo_lock::package::name::Name::from_str(&self.name)?,
+            version: cargo_lock::package::Version::parse(&self.version)?,
+            source: None
+        })
+    }
+}
+
+impl TryInto<cargo_lock::package::Package> for Package {
+    type Error = cargo_lock::error::Error;
+    fn try_into(self) -> Result<cargo_lock::package::Package, Self::Error> {
+        Ok(cargo_lock::package::Package {
+            name: cargo_lock::package::name::Name::from_str(&self.name)?,
+            version: cargo_lock::package::Version::parse(&self.version)?,
+            checksum: Some(cargo_lock::package::checksum::Checksum::from_str(&self.checksum)?),
+            dependencies: vec_try_into::<Dependency, cargo_lock::dependency::Dependency, Self::Error>(self.dependencies)?,
+            replace: None,
+            source: None,
+        })
+    }
+}
+
+impl TryInto<cargo_lock::lockfile::Lockfile> for RawVersionInfo {
+    type Error = cargo_lock::error::Error;
+    fn try_into(self) -> Result<cargo_lock::lockfile::Lockfile, Self::Error> {
+        Ok(cargo_lock::lockfile::Lockfile {
+            version: cargo_lock::lockfile::version::ResolveVersion::V2,
+            // The rant from conversion code from Package also applies here
+            packages: vec_try_into::<Package, cargo_lock::package::Package, Self::Error>(self.packages)?,
+            root: None,
+            metadata: std::collections::BTreeMap::new(),
+            patch: cargo_lock::patch::Patch {unused: Vec::new()},
+        })
+    }
+    
+}
+
+// Surprisingly, TryInto on Vec requires the element to implement TryFrom:
+// https://doc.rust-lang.org/std/vec/struct.Vec.html#impl-TryInto%3CU%3E
+// But we cannot implement TryFrom due to orphan rules.
+// A slightly saner version would be this:
+// self.dependencies.into_iter().map(|x| x.try_into()?).collect()
+// but it also doesn't compile. Hence this mess:
+fn vec_try_into<A: TryInto<B>, B, E: Error + std::convert::From<<A as std::convert::TryInto<B>>::Error>>(source: Vec<A>) -> Result<Vec<B>, E>  {
+        let mut result = Vec::with_capacity(source.len());
+        for dep in source {
+            result.push(dep.try_into()?);
+        }
+        Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
