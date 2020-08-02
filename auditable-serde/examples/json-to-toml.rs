f2ac717c07c9de59fa32e3df7ee539b3ece9860d
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-08-02 18:57:13 +0200

    Fix examples

--- auditable-serde/examples/json-to-toml.rs
+++ auditable-serde/examples/json-to-toml.rs
@@ -7,7 +7,7 @@ fn main() {
     let path = std::env::args().skip(1).next().expect("No file specified");
     let file_contents = std::fs::read_to_string(path).unwrap();
     let version_info = RawVersionInfo::from_str(&file_contents).unwrap();
-    let lockfile: Lockfile = version_info.try_into().unwrap();
+    let lockfile: Lockfile = (&version_info).try_into().unwrap();
     let lockfile_toml = lockfile.to_string();
     let stdout = std::io::stdout();
     let mut stdout = stdout.lock();
