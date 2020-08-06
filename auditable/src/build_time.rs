cc37b89cd6a517dd2e3e951446c66edce347b28e
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-08-06 23:42:36 +0200

    use a use

--- auditable/src/build_time.rs
+++ auditable/src/build_time.rs
@@ -2,13 +2,14 @@
 
 use std::{env, path::{Path, PathBuf}, fs::File, io::Write};
 use auditable_serde::RawVersionInfo;
+use miniz_oxide::deflate::compress_to_vec_zlib;
 
 /// Run this in your build.rs to collect dependency info and make it avaible to `inject_dependency_list!` macro
 pub fn collect_dependency_list() {
     let cargo_lock_contents = load_cargo_lock();
     let version_info = RawVersionInfo::from_toml(&cargo_lock_contents).unwrap();
     let json = serde_json::to_string(&version_info).unwrap();
-    let compressed_json = miniz_oxide::deflate::compress_to_vec_zlib(json.as_bytes(), choose_compression_level());
+    let compressed_json = compress_to_vec_zlib(json.as_bytes(), choose_compression_level());
     let output_file_path = output_file_path();
     write_dependency_info(&compressed_json, &output_file_path);
     export_dependency_file_path(&output_file_path);
