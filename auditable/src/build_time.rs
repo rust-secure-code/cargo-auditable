482bc9547804fa0d7c88c395b1c7b6ad54ba1802
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-08-08 23:59:08 +0200

    Add build-time infrastructure for cargo-metadata conversion

--- auditable/src/build_time.rs
+++ auditable/src/build_time.rs
@@ -1,8 +1,10 @@
 #![cfg(feature = "build_time")]
 
 use std::{env, path::{Path, PathBuf}, fs::File, io::Write};
+use std::collections::HashSet;
 use auditable_serde::RawVersionInfo;
 use miniz_oxide::deflate::compress_to_vec_zlib;
+use cargo_metadata::{Metadata,MetadataCommand};
 
 /// Run this in your build.rs to collect dependency info and make it avaible to `inject_dependency_list!` macro
 pub fn collect_dependency_list() {
@@ -48,4 +50,47 @@ fn choose_compression_level() -> u8 {
         "release" => 7, // not 9 because this also affects speed of incremental builds
         _ => panic!("Unknown build profile: {}", &build_profile)
     }
+}
+
+fn get_metadata() -> Metadata {
+    metadata_command().features(cargo_metadata::CargoOpt::SomeFeatures(enabled_features())).exec().unwrap()
+}
+
+fn metadata_command() -> MetadataCommand {
+    // MetadataCommand::new() automatically reads the $CARGO env var
+    // that Cargo sets for build scripts, so we don't have to pass it explicitly
+    let mut cmd = MetadataCommand::new();
+    let cargo_toml_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("Cargo.toml");
+    cmd.manifest_path(cargo_toml_path);
+    cmd
+}
+
+fn enabled_features() -> Vec<String> {
+    let mut result = Vec::new();
+    // Cargo irreparably mangles the feature list when passing it to the build script
+    // (in particular, case and distinction between `-` and `_` are lost, see
+    // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts)
+    // so we have to reconsruct it by calling cargo-metadata and filtering features
+    // that we know exist against the mangled list of *enabled* features from env variables
+    let enabled_uppercase_features = enabled_uppercase_features();
+    let dry_run_metadata = metadata_command().exec().unwrap();
+    for package in dry_run_metadata.packages {
+        // FIXME: There's no easy way to detect which package we're building, so for now
+        // we just enable everything we've seen in the workspace and hope that this works
+        for (feature, _implied_features) in package.features {
+            let mangled_feature = feature.to_ascii_uppercase().replace("-", "_");
+            if enabled_uppercase_features.contains(&mangled_feature) {
+                result.push(feature);
+            }
+        }
+    }
+    result
+}
+
+fn enabled_uppercase_features() -> HashSet<String> {
+    let mut features = HashSet::new();
+    for (var_name, _value) in env::vars().filter(|(name, _value)| name.len() > "CARGO_FEATURE_".len() && name.starts_with("CARGO_FEATURE_")) {
+        features.insert(var_name.trim_start_matches("CARGO_FEATURE_").to_owned());
+    }
+    features
 }
