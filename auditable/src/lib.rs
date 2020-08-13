9f70b2b6ba3fdf60a5454f86a39ba67ae131a375
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-08-14 00:43:14 +0200

    Fix mAcOs build, hopefully

--- auditable/src/lib.rs
+++ auditable/src/lib.rs
@@ -14,7 +14,11 @@
 macro_rules! inject_dependency_list {
     () => ({
         #[used]
-        #[link_section = ".rust-deps-v0"]
+        #[cfg_attr(target_os = "linux", link_section = ".rust-deps-v0")]
+        #[cfg_attr(target_os = "windows", link_section = ".rust-deps-v0")]
+        #[cfg_attr(target_os = "macos", link_section = "__TEXT.rust-deps-v0")]
+        // All other platforms are not explicitly supported yet and thus don't get any auditable info
+        // It's better to compile on unsupported platforms without audit info than to break compilation
         static AUDITABLE_VERSION_INFO: [u8; include_bytes!(env!("RUST_AUDIT_DEPENDENCY_FILE_LOCATION"))
         .len()] = *include_bytes!(env!("RUST_AUDIT_DEPENDENCY_FILE_LOCATION"));
         &AUDITABLE_VERSION_INFO
