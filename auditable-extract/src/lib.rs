dafb2769759becc98c675d1a58f4c7e8b85c54c9
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-09-04 22:20:05 +0200

    Update elf32 to zero-copy API

--- auditable-extract/src/lib.rs
+++ auditable-extract/src/lib.rs
@@ -7,7 +7,7 @@ pub fn raw_auditable_data<'a>(data: &'a [u8]) -> Option<&'a [u8]> {
     match binfarce::detect_format(data) {
         Format::Elf32{byte_order} => {
             let section = binfarce::elf32::parse(data, byte_order).ok()?
-                .section_with_name(".rust-deps-v0")?;
+                .section_with_name(".rust-deps-v0").ok()??;
                 data.get(section.range().ok()?)
         },
         Format::Elf64{byte_order} => {
