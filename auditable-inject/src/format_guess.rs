ba41c52654032c122ee9bf3ac80164330cba9e22
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-02-22 04:37:49 +0100

    Add format detection from target triple

---
+++ auditable-inject/src/format_guess.rs
@@ -0,0 +1,52 @@
+//! Format guess based on the target triple, e.g. `x86_64-unknown-linux-gnu`
+//! This is hand-rolled and should probably be thrown out when uplifting this into Cargo;
+//! Presumably Cargo already can parse the target triple well enough.
+
+use object::{Architecture, BinaryFormat, Endianness};
+
+pub struct FormatDescription {
+    pub format: BinaryFormat,
+    pub architecture: Architecture,
+    pub endian: Endianness,
+}
+
+pub fn guess_format(target_triple: &str) -> FormatDescription {
+    FormatDescription {
+        format: guess_binary_format(target_triple),
+        architecture: guess_architecture(target_triple),
+        endian: guess_endianness(target_triple),
+    }
+}
+
+fn guess_endianness(target_triple: &str) -> Endianness {
+    Endianness::Little // TODO
+}
+
+fn guess_binary_format(target_triple: &str) -> BinaryFormat {
+    if target_triple.contains("apple") { BinaryFormat::MachO }
+    else if target_triple.contains("windows") { BinaryFormat::Pe }
+    else { BinaryFormat::Elf }
+    // TODO: handle asm.js and wasm somehow
+}
+
+fn guess_architecture(target_triple: &str) -> Architecture {
+    // Referenced from:
+    // https://github.com/rust-lang/rust/blob/3b186511f62b0ce20e72ede0e8e13f8787155f02/compiler/rustc_codegen_ssa/src/back/metadata.rs#L102-L122
+    if target_triple.starts_with("arm") { Architecture::Arm }
+    else if target_triple.starts_with("aarch64") { Architecture::Aarch64 }
+    else if target_triple.starts_with("s390x") { Architecture::S390x }
+    else if target_triple.starts_with("mips") { Architecture::Mips }
+    else if target_triple.starts_with("mips64") { Architecture::Mips64 }
+    else if target_triple.starts_with("x86_64") { Architecture::X86_64 }
+        // TODO - x32 ABI ignored for now
+        // if sess.target.pointer_width == 32 {
+        //     Architecture::X86_64_X32
+        // }
+    else if target_triple.starts_with("x86") { Architecture::I386 }
+    else if target_triple.starts_with("powerpc64") { Architecture::PowerPc64 }
+    else if target_triple.starts_with("powerpc") { Architecture::PowerPc }
+    else if target_triple.starts_with("riscv32") { Architecture::Riscv32 }
+    else if target_triple.starts_with("riscv64") { Architecture::Riscv64 }
+    else if target_triple.starts_with("sparc64") { Architecture::Sparc64 }
+    else { panic!("Unsupported architecture"); }
+}
--- auditable-inject/src/main.rs
+++ auditable-inject/src/main.rs
@@ -1,23 +1,22 @@
 // Shamelessly copied from rustc codebase:
 // https://github.com/rust-lang/rust/blob/e100ec5bc7cd768ec17d75448b29c9ab4a39272b/compiler/rustc_codegen_ssa/src/back/metadata.rs#L233-L287
 
+mod format_guess;
+
 use object::write::{self, StandardSegment, Symbol, SymbolSection};
 use object::{
     elf, Architecture, BinaryFormat, Endianness, FileFlags, Object, ObjectSection, SectionFlags,
     SectionKind, SymbolFlags, SymbolKind, SymbolScope,
 };
 
-pub fn create_compressed_metadata_file(
+use format_guess::{FormatDescription, guess_format};
+
+pub fn create_metadata_file(
     format: &FormatDescription,
     compressed: &[u8],
     symbol_name: &str,
 ) -> Vec<u8> {
-    let mut file = if let Some(file) = create_object_file(format) {
-        file
-    } else {
-        // FIXME: this was copied from rustc codebase but looks sketchy
-        return compressed.to_vec();
-    };
+    let mut file = create_object_file(format);
     let section = file.add_section(
         file.segment_name(StandardSegment::Data).to_vec(),
         b".rustc".to_vec(),
@@ -48,24 +47,13 @@ pub fn create_compressed_metadata_file(
     file.write().unwrap()
 }
 
-fn create_object_file(f: &FormatDescription) -> Option<write::Object<'static>> {
+fn create_object_file(f: &FormatDescription) -> write::Object<'static> {
     // The equivalent function inside rustc contains spooky special-casing for MIPS and RISC-V:
     // https://github.com/rust-lang/rust/blob/03a8cc7df1d65554a4d40825b0490c93ac0f0236/compiler/rustc_codegen_ssa/src/back/metadata.rs#L133-L165
     // I am ignoring that in the prototype for now.
     // To get this into Cargo, presumably we will need a way to share that code between rustc and Cargo.
     // -- Shnatsel
-    let mut file = write::Object::new(f.format, f.architecture, f.endian);
-    Some(file)
-}
-
-pub struct FormatDescription {
-    format: BinaryFormat,
-    architecture: Architecture,
-    endian: Endianness
-}
-
-fn guess_format() -> FormatDescription {
-    todo!();
+    write::Object::new(f.format, f.architecture, f.endian)
 }
 
 fn main() {
