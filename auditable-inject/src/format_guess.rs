8d1058a540481b835f50be660ce468743e354a9f
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-02-23 05:57:40 +0100

    WIP migration to calling rustc to get data; thanks to Nemo157 for the tip

--- auditable-inject/src/format_guess.rs
+++ auditable-inject/src/format_guess.rs
@@ -1,6 +1,4 @@
-//! Format guess based on the target triple, e.g. `x86_64-unknown-linux-gnu`
-//! This is hand-rolled and should probably be thrown out when uplifting this into Cargo;
-//! Presumably Cargo already can parse the target triple well enough.
+use std::io::BufRead;
 
 use object::{Architecture, BinaryFormat, Endianness};
 
@@ -49,4 +47,64 @@ fn guess_architecture(target_triple: &str) -> Architecture {
     else if target_triple.starts_with("riscv64") { Architecture::Riscv64 }
     else if target_triple.starts_with("sparc64") { Architecture::Sparc64 }
     else { panic!("Unsupported architecture"); }
+}
+
+type RustcTargetInfo = std::collections::HashMap<String, String>;
+
+fn rustc_target_info(target_triple: &str) -> RustcTargetInfo {
+    // this is hand-rolled because the relevant piece of Cargo is hideously complex for some reason
+    parse_rustc_target_info(&std::process::Command::new("rustc")
+        .arg("--print=cfg")
+        .arg(format!("target={}", target_triple)) //not being parsed by the shell, so not a vulnerability
+        .output()
+        .expect(&format!("Failed to invoke rustc; make sure it's in $PATH and that '{}' is a valid target triple", target_triple))
+        .stdout)
+}
+
+fn parse_rustc_target_info(rustc_output: &[u8]) -> RustcTargetInfo {
+    // this is split into its own function for unit testing
+    rustc_output
+    .lines()
+    .filter_map(|line| {
+        let line = line.unwrap();
+        // rustc outputs some free-standing values as well as key-value pairs
+        // we're only interested in the pairs, which are separated by '=' and the value is quoted
+        if line.contains("=") {
+            let key = line.split("=").nth(0).unwrap();
+            let mut value: String = line.split("=").skip(1).collect();
+            // strip first and last chars of the quoted value. Verify that they're quotes
+            assert!(value.pop().unwrap() == '"');
+            assert!(value.remove(0) == '"');
+            Some((key.to_owned(), value))
+        } else {
+            None
+        }
+    }).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_rustc_parser_linux() {
+        let rustc_output = br#"debug_assertions
+target_arch="x86_64"
+target_endian="little"
+target_env="gnu"
+target_family="unix"
+target_feature="fxsr"
+target_feature="sse"
+target_feature="sse2"
+target_os="linux"
+target_pointer_width="64"
+target_vendor="unknown"
+unix
+"#;
+        let result = parse_rustc_target_info(rustc_output);
+        assert_eq!(result.get("target_arch").unwrap(), "x86_64");
+        assert_eq!(result.get("target_endian").unwrap(), "little");
+        assert_eq!(result.get("target_pointer_width").unwrap(), "64");
+        assert_eq!(result.get("target_vendor").unwrap(), "unknown");
+    }
 }
