65b6a1119d781ed473be2cc170dcb42afb513d68
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-02-24 05:32:00 +0100

    Initial cargo-auditable prototype

--- cargo-auditable/src/main.rs
+++ cargo-auditable/src/main.rs
@@ -1,5 +1,9 @@
 #![forbid(unsafe_code)]
 
+mod object_file;
+mod target_info;
+mod collect_audit_data;
+
 use cargo_subcommand::Subcommand;
 
 fn main() {
@@ -7,4 +11,21 @@ fn main() {
     // TODO: fix https://github.com/dvc94ch/cargo-subcommand/issues/9, it's a release blocker
     let cmd = Subcommand::new(std::env::args(), "auditable", |_, _| Ok(false)).unwrap();
     println!("{:#?}", cmd);
+
+    // Get the audit data to embed
+    let contents: Vec<u8> = collect_audit_data::compressed_dependency_list(&cmd);
+
+    // TODO: run the code from `auditable-inject` to write the metadata to an object file
+    let target_triple = cmd.target().unwrap_or(cmd.host_triple());
+    let target_info = target_info::rustc_target_info(&target_triple);
+    let binfile = object_file::create_metadata_file(
+        &target_info,
+        &target_triple,
+        &contents,
+        "AUDITABLE_VERSION_INFO", // TODO: make a constant and version it?
+    );
+    // TODO: proper path
+    std::fs::write("audit_data.o", binfile).expect("Unable to write output file");
+
+    // TODO: set the RUSTFLAGS environment variable and call Cargo with all the Cargo args
 }
