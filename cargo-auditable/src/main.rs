ad289f51c7d5a05530d1df9a5c98ee6281ff60a7
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-02-24 04:34:12 +0100

    Don't enable default features on `object` crate

--- cargo-auditable/src/main.rs
+++ cargo-auditable/src/main.rs
@@ -4,6 +4,7 @@ use cargo_subcommand::Subcommand;
 
 fn main() {
     // TODO: refactor cargo-subcommand to use os_args and OsStr types. Paths can be non-UTF-8 on most platforms.
+    // TODO: fix https://github.com/dvc94ch/cargo-subcommand/issues/9, it's a release blocker
     let cmd = Subcommand::new(std::env::args(), "auditable", |_, _| Ok(false)).unwrap();
     println!("{:#?}", cmd);
 }
