c497a24567b8e03bee63cd33c492f45280aa1539
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-02-24 04:12:02 +0100

    Parse basic args

--- cargo-auditable/src/main.rs
+++ cargo-auditable/src/main.rs
@@ -1,5 +1,9 @@
 #![forbid(unsafe_code)]
 
+use cargo_subcommand::Subcommand;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: refactor cargo-subcommand to use os_args and OsStr types. Paths can be non-UTF-8 on most platforms.
+    let cmd = Subcommand::new(std::env::args(), "auditable", |_, _| Ok(false)).unwrap();
+    println!("{:#?}", cmd);
 }
