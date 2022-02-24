6a5c0818a6324c43066b28941dd1adba8980ce2a
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-02-24 11:16:09 +0100

    Add setting env vars and calling cargo to `cargo auditable`; it can now function end-to-end

--- cargo-auditable/src/main.rs
+++ cargo-auditable/src/main.rs
@@ -5,12 +5,13 @@ mod target_info;
 mod collect_audit_data;
 
 use cargo_subcommand::Subcommand;
+use std::{process::Command, ffi::OsString};
 
 fn main() {
     // TODO: refactor cargo-subcommand to use os_args and OsStr types. Paths can be non-UTF-8 on most platforms.
     // TODO: fix https://github.com/dvc94ch/cargo-subcommand/issues/9, it's a release blocker
     let cmd = Subcommand::new(std::env::args(), "auditable", |_, _| Ok(false)).unwrap();
-    println!("{:#?}", cmd);
+    //println!("{:#?}", cmd);
 
     // Get the audit data to embed
     let contents: Vec<u8> = collect_audit_data::compressed_dependency_list(&cmd);
@@ -28,4 +29,33 @@ fn main() {
     std::fs::write("audit_data.o", binfile).expect("Unable to write output file");
 
     // TODO: set the RUSTFLAGS environment variable and call Cargo with all the Cargo args
+    let mut command = cargo_command(&cmd);
+    command.env("RUSTFLAGS", rustflags_with_audit_object());
+    let results = command.status().expect("Failed to invoke cargo! Make sure it's in your $PATH");
+    std::process::exit(results.code().unwrap());
+}
+
+/// Creates a cargo command line and populates arguments from arguments passed to `cargo auditable`
+/// Does not read or modify environment variables.
+fn cargo_command(cargo_auditable_args: &Subcommand) -> Command {
+    let mut command = Command::new("cargo");
+    // subcommand such as "build", "run", "check", "test"
+    command.arg(cargo_auditable_args.cmd()); // TODO: prohibit recursion. Someone could put "auditable" here, and the build will fail.
+    // Pass along all our arguments; we don't currently have any args specific to `cargo auditable`
+    for arg in cargo_auditable_args.args() {
+        command.arg(arg);
+    }
+    command
+}
+
+const OUR_RUSTFLAGS: &str = " -Clink-arg=audit_data.o -Clink-arg=-Wl,--require-defined=AUDITABLE_VERSION_INFO ";
+
+fn rustflags_with_audit_object() -> OsString {
+    match std::env::var_os("RUSTFLAGS") {
+        None => OUR_RUSTFLAGS.into(),
+        Some(mut val) => {
+            val.push(OUR_RUSTFLAGS);
+            val
+        },
+    }
 }
