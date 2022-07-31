ff01ec39386f39ba86fe2db215de4b1c3a4b5443
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-07-31 20:34:32 +0200

    Drop the hacky build.rs-based approach; only use `cargo auditable`

--- hello-auditable/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    auditable_build::collect_dependency_list();
-}
--- hello-auditable/src/main.rs
+++
@@ -1,9 +0,0 @@
-static COMPRESSED_DEPENDENCY_LIST: &[u8] = auditable::inject_dependency_list!();
-
-fn main() {
-    println!("Hello, world!");
-    // Actually use the data to work around a bug in rustc:
-    // https://github.com/rust-lang/rust/issues/47384
-    // On nightly you can use `test::black_box` instead of `println!`
-    println!("{}", COMPRESSED_DEPENDENCY_LIST[0]);
-}
