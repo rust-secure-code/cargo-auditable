ac14c6114ccb87f35df24e068526bfee0faff77e
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-07-31 23:20:50 +0200

    Revert "Remove a redundant check from rustc_wrapper" - it broke something
    This reverts commit 669d8e751e111124454a421b7a5e52733291e0c1.

--- cargo-auditable/src/rustc_wrapper.rs
+++ cargo-auditable/src/rustc_wrapper.rs
@@ -5,42 +5,41 @@ use crate::{collect_audit_data, target_info, object_file, rustc_arguments};
 use std::io::BufRead;
 
 pub fn main() {
-    // This code is called only if RUSTC_WORKSPACE_WRAPPER is set,
-    // so it is only called for crates in the current workspace,
-    // or the sole local crate being built if there's no workspace.
-
     let mut command = rustc_command();
 
-    let args = rustc_arguments::parse_args().unwrap();
+    // if this is a primary package in our workspace, inject the audit data
+    if let Some(_) = env::var_os("CARGO_PRIMARY_PACKAGE") {
+        let args = rustc_arguments::parse_args().unwrap();
 
-    // Only inject arguments into crate types 'bin' and 'cdylib'
-    // What if there are multiple types, you might ask? I have no idea!
-    // TODO: check if crates that are both rlib and bin actually work
-    if args.crate_types.contains(&"bin".to_owned()) || args.crate_types.contains(&"cdylib".to_owned()) {
-        // Get the audit data to embed
-        let contents: Vec<u8> = collect_audit_data::compressed_dependency_list(&args);
-        // write the audit info to an object file
-        let target_triple = args.target.unwrap_or(rustc_host_target_triple());
-        let target_info = target_info::rustc_target_info(&target_triple);
-        let binfile = object_file::create_metadata_file(
-            &target_info,
-            &target_triple,
-            &contents,
-            "AUDITABLE_VERSION_INFO", // TODO: make a constant and version it?
-        );
-        // Place the audit data in the output dir.
-        // We can place it anywhere really, the only concern is clutter and name collisions,
-        // and the target dir is locked so we're probably good
-        let filename = format!("{}_audit_data.o", args.crate_name);
-        let path = args.out_dir.clone().join(filename);
-        std::fs::write(&path, binfile).expect("Unable to write output file");
+        // Only inject arguments into crate types 'bin' and 'cdylib'
+        // What if there are multiple types, you might ask? I have no idea!
+        // TODO: check if crates that are both rlib and bin actually work
+        if args.crate_types.contains(&"bin".to_owned()) || args.crate_types.contains(&"cdylib".to_owned()) {
+            // Get the audit data to embed
+            let contents: Vec<u8> = collect_audit_data::compressed_dependency_list(&args);
+            // write the audit info to an object file
+            let target_triple = args.target.unwrap_or(rustc_host_target_triple());
+            let target_info = target_info::rustc_target_info(&target_triple);
+            let binfile = object_file::create_metadata_file(
+                &target_info,
+                &target_triple,
+                &contents,
+                "AUDITABLE_VERSION_INFO", // TODO: make a constant and version it?
+            );
+            // Place the audit data in the output dir.
+            // We can place it anywhere really, the only concern is clutter and name collisions,
+            // and the target dir is locked so we're probably good
+            let filename = format!("{}_audit_data.o", args.crate_name);
+            let path = args.out_dir.clone().join(filename);
+            std::fs::write(&path, binfile).expect("Unable to write output file");
 
-        // Modify the rustc command to link the object file with audit data
-        let mut linker_command = OsString::from("-Clink-arg=");
-        linker_command.push(&path);
-        command.arg(linker_command);
-        // Prevent the symbol from being removed as unused by the linker
-        command.arg("-Clink-arg=-Wl,--require-defined=AUDITABLE_VERSION_INFO");
+            // Modify the rustc command to link the object file with audit data
+            let mut linker_command = OsString::from("-Clink-arg=");
+            linker_command.push(&path);
+            command.arg(linker_command);
+            // Prevent the symbol from being removed as unused by the linker
+            command.arg("-Clink-arg=-Wl,--require-defined=AUDITABLE_VERSION_INFO");
+        }
     }
 
     // Invoke rustc
