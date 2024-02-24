b34d6fc85acfce12787d6a593d207d80712eeebc
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-24 01:20:00 +0000

    Initial implementation of auditable-to-cyclonedx conversion

--- auditable-cyclonedx/src/lib.rs
+++ auditable-cyclonedx/src/lib.rs
@@ -1,14 +1,64 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use auditable_serde;
+use auditable_serde::Package;
+pub use cyclonedx_bom;
+
+use cyclonedx_bom::models::{component::Classification, component::Component, metadata::Metadata};
+use cyclonedx_bom::prelude::*;
+
+/// Converts the metadata embedded by `cargo auditable` to a minimal CycloneDX document
+/// that is heavily optimized to reduce the size
+pub fn auditable_to_minimal_cdx(input: &auditable_serde::VersionInfo) -> Bom {
+    let mut bom = Bom::default();
+    // The toplevel component goes into its own field, as per the spec:
+    // https://cyclonedx.org/docs/1.5/json/#metadata_component
+    let (root_idx, root_pkg) = root_package(input);
+    let root_component = pkg_to_component(root_pkg, root_idx);
+    let mut metadata = Metadata::default();
+    metadata.component = Some(root_component);
+    bom.metadata = Some(metadata);
+    // Fill in the component list, excluding the toplevel component (already encoded)
+    let components: Vec<Component> = input
+        .packages
+        .iter()
+        .enumerate()
+        .filter(|(_idx, pkg)| !pkg.root)
+        .map(|(idx, pkg)| pkg_to_component(pkg, idx))
+        .collect();
+    let components = Components(components);
+    bom.components = Some(components);
+    // TODO: dependency tree
+    if cfg!(debug_assertions) {
+        assert_eq!(bom.validate(), ValidationResult::Passed);
+    }
+    bom
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn pkg_to_component(pkg: &auditable_serde::Package, idx: usize) -> Component {
+    let component_type = if pkg.root {
+        Classification::Application
+    } else {
+        Classification::Library
+    };
+    // The only requirement for `bom_ref` according to the spec is that it's unique,
+    // so we just keep the unique numbering already used in the original
+    let bom_ref = idx.to_string();
+    Component::new(
+        component_type,
+        &pkg.name,
+        &pkg.version.to_string(),
+        Some(bom_ref),
+    )
+    // TODO: source
+    // TODO: dependency kind
+    // TODO: purl
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn root_package(input: &auditable_serde::VersionInfo) -> (usize, &Package) {
+    // we can unwrap here because VersionInfo is already validated during deserialization
+    input
+        .packages
+        .iter()
+        .enumerate()
+        .find(|(_idx, pkg)| pkg.root)
+        .expect("VersionInfo contains no root package!")
 }
