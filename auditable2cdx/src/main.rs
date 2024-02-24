b569ddcc89e03463e77236958ec9a7f078149caa
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-24 03:05:14 +0000

    Prototype impl of auditable2cdx

--- auditable2cdx/src/main.rs
+++ auditable2cdx/src/main.rs
@@ -1,3 +1,14 @@
+use std::path::Path;
+
+use auditable_cyclonedx::auditable_to_minimal_cdx;
+use auditable_info::audit_info_from_file;
+
 fn main() {
-    println!("Hello, world!");
+    let input_filename = std::env::args_os()
+        .nth(1)
+        .expect("No input file specified!");
+    let info = audit_info_from_file(Path::new(&input_filename), Default::default()).unwrap();
+    let cyclonedx = auditable_to_minimal_cdx(&info);
+    let mut stdout = std::io::stdout().lock();
+    cyclonedx.output_as_json_v1_3(&mut stdout).unwrap();
 }
