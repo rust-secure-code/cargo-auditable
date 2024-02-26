968107029bddcfc2d2c70cc6b0a14880e29e4a00
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-26 05:26:46 +0000

    Work around cyclonedx-bom limitations to produce minified JSON

---
+++ auditable2cdx/src/workarounds.rs
@@ -0,0 +1,25 @@
+use serde_json;
+
+/// Accepts BOM in JSON and minifies it,
+/// working around https://github.com/CycloneDX/cyclonedx-rust-cargo/issues/628
+pub fn minify_bom(bom: &[u8]) -> String {
+    let mut json: serde_json::Value = serde_json::from_slice(bom).unwrap();
+    // clear the unnecessary toplevel fields
+    let toplevel = json.as_object_mut().unwrap();
+    toplevel.remove("version");
+    toplevel.remove("serialNumber");
+    // clear empty arrays in dependencies
+    if let Some(deps) = toplevel.get_mut("dependencies") {
+        let deps = deps.as_array_mut().unwrap();
+        deps.iter_mut().for_each(|dependency| {
+            if let Some(deps_array) = dependency.get("dependsOn") {
+                let deps_array = deps_array.as_array().unwrap();
+                if deps_array.is_empty() {
+                    dependency.as_object_mut().unwrap().remove("dependsOn");
+                }
+            }
+        });
+    }
+    // .to_string() writes the minified JSON, unlike .to_string_pretty()
+    serde_json::to_string(&json).unwrap()
+}
