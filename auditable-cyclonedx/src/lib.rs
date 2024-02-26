44e1abc26d34f2a6eec658963b13ab9f5673f5dd
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-26 04:18:53 +0000

    cyclonedx-bom: also record PURL

--- auditable-cyclonedx/src/lib.rs
+++ auditable-cyclonedx/src/lib.rs
@@ -1,16 +1,21 @@
 #![forbid(unsafe_code)]
 
+use std::str::FromStr;
+
 pub use auditable_serde;
-use auditable_serde::Package;
+use auditable_serde::{Package, Source};
 pub use cyclonedx_bom;
 
-use cyclonedx_bom::models::{
-    component::Classification,
-    component::Component,
-    dependency::{Dependencies, Dependency},
-    metadata::Metadata,
-};
 use cyclonedx_bom::prelude::*;
+use cyclonedx_bom::{
+    external_models::uri::Purl,
+    models::{
+        component::Classification,
+        component::Component,
+        dependency::{Dependencies, Dependency},
+        metadata::Metadata,
+    },
+};
 
 /// Converts the metadata embedded by `cargo auditable` to a minimal CycloneDX document
 /// that is heavily optimized to reduce the size
@@ -69,15 +74,18 @@ fn pkg_to_component(pkg: &auditable_serde::Package, idx: usize) -> Component {
     // The only requirement for `bom_ref` according to the spec is that it's unique,
     // so we just keep the unique numbering already used in the original
     let bom_ref = idx.to_string();
-    Component::new(
+    let mut result = Component::new(
         component_type,
         &pkg.name,
         &pkg.version.to_string(),
         Some(bom_ref),
-    )
-    // TODO: source
+    );
+    // PURL encodes the package origin (registry, git, local) - sort of, anyway
+    let purl = purl(&pkg);
+    let purl = Purl::from_str(&purl).unwrap();
+    result.purl = Some(purl);
     // TODO: dependency kind
-    // TODO: purl
+    result
 }
 
 fn root_package(input: &auditable_serde::VersionInfo) -> (usize, &Package) {
@@ -88,4 +96,25 @@ fn root_package(input: &auditable_serde::VersionInfo) -> (usize, &Package) {
         .enumerate()
         .find(|(_idx, pkg)| pkg.root)
         .expect("VersionInfo contains no root package!")
+}
+
+fn purl(pkg: &auditable_serde::Package) -> String {
+    // The purl crate exposed by `cyclonedx-bom` doesn't support the qualifiers we need,
+    // so we just build the PURL as a string.
+    // Yeah, we could use *yet another* dependency to build the PURL,
+    // but we use it such trivial ways that it isn't worth the trouble.
+    // Specifically, the crate names that crates.io accepts don't need percent-encoding
+    // and the fixed values we put in arguments don't either
+    // (but percent-encoding is underspecified and not interoperable anyway,
+    // see e.g. https://github.com/package-url/purl-spec/pull/261)
+    let mut purl = format!("pkg:cargo/{}@{}", pkg.name, pkg.version);
+    purl.push_str(match &pkg.source {
+        Source::CratesIo => "", // this is the default, nothing to qualify
+        Source::Git => "&vcs_url=redacted",
+        Source::Local => "&download_url=redacted",
+        Source::Registry => "&repository_url=redacted",
+        Source::Other(_) => "&download_url=redacted",
+        unknown => panic!("Unknown source: {:?}", unknown),
+    });
+    purl
 }
