22cbea3e519f55e22ac41b06828d733b320c9e7c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-05-24 23:26:45 +0100

    Add a currently-failing test for dependency unification

--- cargo-auditable/tests/it.rs
+++ cargo-auditable/tests/it.rs
@@ -464,3 +464,24 @@ fn test_wasm() {
     eprintln!("wasm_crate.wasm dependency info: {dep_info:?}");
     assert_eq!(dep_info.packages.len(), 19);
 }
+
+#[test]
+fn test_dependency_unification() {
+    // Path to workspace fixture Cargo.toml. See that file for overview of workspace members and their dependencies.
+    let workspace_cargo_toml = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("tests/fixtures/runtime_and_dev_dep_with_different_features/Cargo.toml");
+    // Run in workspace root with default features
+    let bins = run_cargo_auditable(workspace_cargo_toml, &[], &[]);
+    eprintln!("Test fixture binary map: {bins:?}");
+
+    // check that the build types are propagated correctly
+    let toplevel_crate_bin = &bins.get("top_level_crate").unwrap()[0];
+    let dep_info = get_dependency_info(toplevel_crate_bin);
+    eprintln!("{toplevel_crate_bin} dependency info: {dep_info:?}");
+    assert!(dep_info.packages.len() == 2);
+    // optional_transitive_dep should not be present
+    assert!(!dep_info
+        .packages
+        .iter()
+        .any(|p| p.name == "optional_transitive_dep"));
+}
